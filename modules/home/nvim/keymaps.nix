5a05fe53207d6bad024d668786df6aab0b36f8f1
breanna <52141078+breanna-rayburn@users.noreply.github.com>
2026-02-12 14:25:47 -0800

    keymaps in telescope and ripgrep

--- modules/home/nvim/keymaps.nix
+++ modules/home/nvim/keymaps.nix
@@ -2,12 +2,4 @@
 
 {
   globals.mapleader = " ";
-
-  keymaps = [
-    {
-      mode = "n";
-      action = "<cmd>Telescope find_files<CR>";
-      key = "<leader>f";
-    }
-  ];
 }
--- modules/home/nvim/plugins/telescope.nix
+++ modules/home/nvim/plugins/telescope.nix
@@ -2,9 +2,30 @@
 
 {
   plugins = {
+    #devicons auto-enable when telescope enable?
     web-devicons.enable = false;
     telescope = {
       enable = true;
-    };
+      #adds ripgrep dependency
+      extensions.live-grep-args.enable = true;
+    };  
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      action = "<cmd>Telescope find_files<CR>";
+      key = "<leader>f";
+    }
+    {
+      mode = "n";
+      action = "<cmd>Telescope live_grep<CR>";
+      key = "<leader>lg";
+    }
+    {
+      mode = "n";
+      action = "<cmd>Telescope grep_string<CR>";
+      key = "<leader>ls";
+    } 
+  ];
 }
